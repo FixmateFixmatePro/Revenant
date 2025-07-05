94c80c29545a8d772b59d8cfa34d5964fb2d5ad3
FixmateFixmatePro <FixMateAITradesTool@gmail.com>
2025-07-04 20:10:26 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/revenant-one-site/',
+  base: 'https://fixmatefixmatepro.github.io/Reverent-One/',
   plugins: [react()],
 })
