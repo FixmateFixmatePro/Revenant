6bd2a3d32939e3c327d808b4e12ce89111127e9d
FixmateFixmatePro <FixMateAITradesTool@gmail.com>
2025-07-05 17:50:22 -0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: 'https://fixmatefixmatepro.github.io/Reverent-One/',
-  plugins: [react()],
-})
